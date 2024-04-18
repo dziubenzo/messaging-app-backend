03d79c8afd395344a79c469bab28b3da82ae0af1
dziubenzo <dziubenzo@gmail.com>
2024-04-18 11:17:35 +0200

    Build POST create message route

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -20,15 +20,59 @@ export const getMessages = [
     const from = req.query.from;
     const to = req.query.to;
 
+    // Get messages from user A to user B and from user B to user A
     const [messagesSent, messagesReceived] = await Promise.all([
       Message.find({ sender: from, recipient: to }).lean().exec(),
       Message.find({ sender: to, recipient: from }).lean().exec(),
     ]);
 
-    if (!messagesSent.length || !messagesReceived.length) {
+    // Return a 404 message if no messages found in the DB
+    if (!messagesSent.length && !messagesReceived.length) {
       return res.json('No messages to show.');
     }
 
-    return res.json('OK');
+    // Return messages
+    return res.json({ messagesSent, messagesReceived });
+  }),
+];
+
+// POST create message
+export const postCreateMessage = [
+  body('sender')
+    .isMongoId()
+    .withMessage('Sender field must be a valid Mongo ID'),
+  body('recipient')
+    .isMongoId()
+    .withMessage('Recipient field must be a valid Mongo ID'),
+  body('text')
+    .trim()
+    .isLength({ min: 1, max: 5000 })
+    .withMessage('Message must be present and cannot exceed 5000 characters'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // Return the first validation error message if there are any errors
+      const firstErrorMsg = getFirstErrorMsg(errors);
+      return res.status(400).json(firstErrorMsg);
+    }
+
+    const sender = req.body.sender;
+    const recipient = req.body.recipient;
+    const text = req.body.text;
+
+    // Create new message with current date
+    const newMessage = new Message({
+      sender,
+      recipient,
+      text,
+      date: Date.now(),
+    });
+
+    await newMessage.save();
+
+    // Return new message
+    return res.json(newMessage);
   }),
 ];
--- routes/message.js
+++ routes/message.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
-import { getMessages } from '../controllers/messageController.js';
+import {
+  getMessages,
+  postCreateMessage,
+} from '../controllers/messageController.js';
 import { isAuth } from '../config/passport.js';
 
 const router = Router();
@@ -8,5 +11,8 @@ router.use(isAuth);
 
 // GET messages user A <> user B
 router.get('/', getMessages);
+
+// POST create message
+router.post('/', postCreateMessage);
 
 export default router;
