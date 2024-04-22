dd696f17bbf327d46998f9967bbbb6cf036702f1
dziubenzo <dziubenzo@gmail.com>
2024-04-22 12:17:32 +0200

    Build DELETE group chat route

--- controllers/groupChatController.js
+++ controllers/groupChatController.js
@@ -1,7 +1,7 @@
 import GroupChat from '../models/GroupChat.js';
 
 import asyncHandler from 'express-async-handler';
-import { body, query, validationResult } from 'express-validator';
+import { body, param, query, validationResult } from 'express-validator';
 import { getFirstErrorMsg } from '../config/helpers.js';
 import { checkNameAvailability } from '../config/middleware.js';
 
@@ -65,5 +65,26 @@ export const postCreateGroupChat = [
     await newGroupChat.save();
 
     return res.json('Group chat created successfully!');
+  }),
+];
+
+// DELETE group chat
+export const deleteGroupChat = [
+  param('groupChatId').isMongoId().withMessage('Invalid URL parameter'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // Return the first validation error message if there are any errors
+      const firstErrorMsg = getFirstErrorMsg(errors);
+      return res.status(400).json(firstErrorMsg);
+    }
+
+    const groupChatId = req.params.groupChatId;
+
+    const { name } = await GroupChat.findByIdAndDelete(groupChatId);
+    
+    return res.json(`Group chat ${name} deleted successfully!`);
   }),
 ];
--- routes/groupChat.js
+++ routes/groupChat.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   getGroupChats,
   postCreateGroupChat,
+  deleteGroupChat,
 } from '../controllers/groupChatController.js';
 import { isAuth } from '../config/passport.js';
 
@@ -14,5 +15,8 @@ router.get('/', getGroupChats);
 
 // POST create group chat
 router.post('/', postCreateGroupChat);
+
+// DELETE group chat
+router.delete('/:groupChatId', deleteGroupChat);
 
 export default router;
